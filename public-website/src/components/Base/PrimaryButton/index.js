30a3380d7a3c708fb1b65fd8b8a70fe7ff0d8835
Kevin Fabre <kevin.fabre@gemmici.com>
2020-11-19 18:31:31 +0100

    edit style for primary button

--- public-website/src/components/Base/PrimaryButton/index.js
+++ public-website/src/components/Base/PrimaryButton/index.js
@@ -1,13 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 
-const BasePrimaryButton = ({ text, className }) => (
-  <button type="button" className={`h-14 bg-action ${className}`}>
-    <div className="px-12 flex flex-row justify-center items-center">
-      <span className="font-serif font-bold text-white text-xl leading-none tracking-wider uppercase">
-        {text}
-      </span>
-    </div>
-  </button>
-)
+const BasePrimaryButton = ({ text, className }) => {
+  const [isButtonHover, setButtonIsHover] = useState(false)
+
+  return (
+    <button
+      type="button"
+      onMouseEnter={() => setButtonIsHover(true)}
+      onMouseLeave={() => setButtonIsHover(false)}
+      className={`h-14 bg-action ${className} relative ${
+        isButtonHover ? "bottom-0 right-0" : "bottom-1 right-1"
+      } transition-position duration-300 ease-in-out`}
+    >
+      <div className="px-12 flex flex-row justify-center items-center">
+        <span className="font-serif font-bold text-white text-xl leading-none tracking-wider uppercase subpixel-antialiased">
+          {text}
+        </span>
+      </div>
+      <div
+        className={`absolute transition-position duration-300 ease-in-out ${
+          isButtonHover ? "inset-0" : "inset-1"
+        } w-full h-full border border-action border-solid`}
+      ></div>
+    </button>
+  )
+}
 
 export default BasePrimaryButton
--- public-website/tailwind.config.js
+++ public-website/tailwind.config.js
@@ -2,22 +2,25 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   theme: {
-    inset: {
-      1: "0.25rem",
-    },
     extend: {
+      colors: {
+        primary: "#5E4F41",
+        action: "#996515",
+        background: "#FAF6F1",
+      },
       fontFamily: {
         serif: ["Judson", ...defaultTheme.fontFamily.serif],
         sans: ["Josefin Sans", ...defaultTheme.fontFamily.sans],
+      },
+      inset: {
+        1: "0.25rem",
       },
       spacing: {
         14: "3.5rem",
         26: "6,5rem",
       },
-      colors: {
-        primary: "#5E4F41",
-        action: "#996515",
-        background: "#FAF6F1",
+      transitionProperty: {
+        position: "top, right, bottom, left",
       },
     },
   },
