f27626d1705fabb8a9932562b2824f131cc2f384
Justine <hell.justine@gmail.com>
2020-11-22 16:44:11 +0100

    add MobileNav for responsive Menu

--- public-website/src/components/header.js
+++ public-website/src/components/header.js
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 
 import BasePrimaryButton from "../components/Base/PrimaryButton/index"
 import BaseTertiaryButton from "../components/Base/TertiaryButton/index"
 import Logo from "../components/Logo/index"
+import MobileNav from "./MobileNav"
 
 import { HiOutlineMenu } from "react-icons/hi"
 
 import { useTranslation } from "react-i18next"
 
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClickBurgerMenu = () => {
+    setIsClicked(prevState => !prevState)
+    document.querySelector("body").style.overflowY = isClicked
+      ? "scroll"
+      : "hidden"
+  }
   const { t } = useTranslation()
+
   return (
-    <header className="z-50 fixed top-0 left-0 right-0 xs:px-6 px-12 bg-background">
+    <header className="z-40 fixed top-0 left-0 right-0 xs:px-6 px-12 bg-background">
       <div className="py-6 flex flex-row items-center justify-between">
         <Logo />
         <section className="xs:hidden md:flex flex-row items-center">
@@ -29,9 +39,16 @@ const Header = () => {
           />
         </section>
         <button type="button" className="xs:block hidden">
-          <HiOutlineMenu className="h-6 w-6 text-action" />
+          <HiOutlineMenu
+            className="h-6 w-6 text-action"
+            onClick={handleClickBurgerMenu}
+          />
         </button>
       </div>
+      <MobileNav
+        className={isClicked ? "block" : "hidden"}
+        handleClickBurgerMenu={handleClickBurgerMenu}
+      />
     </header>
   )
 }
