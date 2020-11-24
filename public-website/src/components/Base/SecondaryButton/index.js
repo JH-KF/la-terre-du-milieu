8481bd0cdc4d4a81242df9679e5a1a58251fac66
Kevin Fabre <kevin.fabre@gemmici.com>
2020-11-24 16:50:13 +0100

    add bg white to secondary button

--- public-website/src/components/Base/SecondaryButton/index.js
+++ public-website/src/components/Base/SecondaryButton/index.js
@@ -3,7 +3,7 @@ import React from "react"
 const BaseSecondaryButton = ({ text, className }) => (
   <button
     type="button"
-    className={`h-14 border border-solid border-action ${className}`}
+    className={`h-14 bg-white border border-solid border-action ${className}`}
   >
     <div className="px-12 flex flex-row justify-items-center item-center">
       <span className="font-serif text-action text-xl">{text}</span>
