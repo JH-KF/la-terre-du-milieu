d2fab7e647219cd54621437c8a83386f31b85c97
Kevin Fabre <kevin.fabre@gemmici.com>
2020-11-29 14:28:36 +0100

    edit gasby config to do translation during built time

--- public-website/gatsby-ssr.js
+++ public-website/gatsby-ssr.js
@@ -1,7 +1,38 @@
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ * Implement Gatsby's Browser APIs in this file.
  *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
+ * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
 // You can delete this file if you're not using it
+import "./src/css/index.css"
+
+import React from "react"
+import Layout from "./src/components/layout.js"
+
+import i18n from "./src/config/i18next.js"
+import { I18nextProvider } from "react-i18next"
+import { PageContextProvider } from "./src/context/pageContext"
+import { BookModalContextProvider } from "./src/context/bookModalContext"
+
+/**
+ * Wrap all pages with a Translation provider and set the language on SSR time
+ */
+export const wrapRootElement = ({ element }) => {
+  return (
+    <I18nextProvider i18n={i18n}>
+      <BookModalContextProvider>{element}</BookModalContextProvider>
+    </I18nextProvider>
+  )
+}
+
+/**
+ * Wrap all pages with a Translation provider and set the language on SSR time
+ */
+export const wrapPageElement = ({ element, props }) => {
+  return (
+    <PageContextProvider pageContext={props.pageContext}>
+      <Layout {...props}>{element}</Layout>
+    </PageContextProvider>
+  )
+}
--- public-website/src/config/i18next.js
+++ public-website/src/config/i18next.js
@@ -1,7 +1,10 @@
 import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+
 import translations from "./translations"
 
-i18n.init({
+i18n.use(initReactI18next).init({
+  lng: ["fr", "en"],
   resources: translations,
   fallbackLng: "fr",
   interpolation: {
