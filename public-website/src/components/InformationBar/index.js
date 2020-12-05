bd8dd4a390bef5ca298e0d55be86d944d056924f
Kevin Fabre <kevin.fabre@gemmici.com>
2020-12-05 12:45:49 +0100

    add a custom background for the information bar

--- public-website/src/components/InformationBar/index.js
+++ public-website/src/components/InformationBar/index.js
@@ -10,7 +10,7 @@ const InformationBar = ({ informations }) => {
     <nav
       className={`${
         isClicked ? "hidden" : "block"
-      } z-30 bg-background-accent py-2 px-12 xs:px-6 shadow`}
+      } z-30 py-2 px-12 xs:px-6 information-bar relative`}
     >
       <div className="max-w-screen-xl m-auto relative pr-6 flex justify-center items-center">
         <BaseDescription description={informations} />
