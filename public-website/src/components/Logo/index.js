18223e6a4d53df1b610a735117151a6b1c6234d1
Kevin Fabre <kevin.fabre@gemmici.com>
2021-01-11 17:42:24 +0100

    add link to the logo

--- public-website/src/components/Logo/index.js
+++ public-website/src/components/Logo/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
+import i18n from "i18next"
 
 const Logo = () => {
   const queryData = useStaticQuery(graphql`
@@ -16,16 +18,19 @@ const Logo = () => {
     }
   `)
   const { t } = useTranslation()
+  const logoLink = i18n.language === "fr" ? "/" : `/${i18n.language}/`
   return (
-    <div className="flex items-center">
-      <Img
-        fluid={queryData.logo.childImageSharp.fluid}
-        className="h-12 w-12 mr-4 flex-shrink-0"
-      ></Img>
-      <span className="text-gray-800 lg:text-2xl md:text-xl xs:text-xl xs:leading-6 font-calligraphy font-bold">
-        {t("siteMetadata.title")}
-      </span>
-    </div>
+    <Link to={logoLink}>
+      <div className="flex items-center">
+        <Img
+          fluid={queryData.logo.childImageSharp.fluid}
+          className="h-12 w-12 mr-4 flex-shrink-0"
+        ></Img>
+        <span className="text-gray-800 lg:text-2xl md:text-xl xs:text-xl xs:leading-6 font-calligraphy font-bold">
+          {t("siteMetadata.title")}
+        </span>
+      </div>
+    </Link>
   )
 }
 
